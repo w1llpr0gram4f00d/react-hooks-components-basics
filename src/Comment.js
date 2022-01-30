92df9f85c343f567720df430cc4b01508a9cf7f5
Marc Cavazos <marcavazos19@icloud.com>
2022-01-30 12:11:40 -0500

    commit tree e4dd18cee3e6ab60173087949158c4f6f41c2167

--- src/Comment.js
+++ src/Comment.js
@@ -4,4 +4,11 @@ function Comment() {
   return <div>Naturally, I agree with this article.</div>;
 }
 
+function Article() {
+  return ( 
+    <div>Dear Reader: Bjarne Stroustrup has the perfect lecture oration.</div>
+  );
+}
+
 export default Comment;
+export default Article;
